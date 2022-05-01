411235cb2eadd94b14b011b38c3e854422368097
vamsichaganti <vamsichaganti@dbs.com>
2022-05-01 15:50:32 +0530

    spring cloud config server using git demo

---
+++ src/test/java/com/vkc/configserver/SpringConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vkc.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
